011b96fd1d04db29a0a27a1f571b6e48d259591d
Manal Mrabet <manal.m-rabet-el-khomssi@etu.unistra.fr>
2025-05-14 00:05:26 +0200

    Ajout de la documentation Sphinx

---
+++ source/conf.py
@@ -0,0 +1,61 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Pile IgH documentation'
+copyright = '2025'
+author = 'Emma DELAHAIE et Manal MRABET EL KHOMSSI'
+release = '1.0.0'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    "sphinx.ext.graphviz",
+    "sphinx.ext.ifconfig",
+    "sphinx.ext.intersphinx",
+    "sphinx_copybutton",
+    "sphinx_tabs.tabs",
+    "sphinx_rtd_theme",
+    "myst_parser",
+    "sphinx_design",
+]
+
+myst_enable_extensions = ["colon_fence"]
+
+templates_path = ['_templates']
+
+language = 'french'
+
+
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store"]
+source_suffix = [".rst", ".md"]
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_sidebars = {
+    '**': [
+        'globaltoc.html',   # Use the global TOC instead of the local one
+        'relations.html',   # Adds links to the previous and next chapters
+        'searchbox.html',   # Adds a search box
+    ]
+}
+
+
+# -- Options for copybutton extension ----------------------------------------
+copybutton_prompt_text = r">>> |\.\.\. |\$ |In \[\d*\]: | {2,5}\.\.\.: | {5,8}: "
+copybutton_prompt_is_regexp = True
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+html_css_files = [
+    "css/custom.css",
+]
